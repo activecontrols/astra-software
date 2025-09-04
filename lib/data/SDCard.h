1f45b5891a4e6b3c679c43096175edb839aeb5b9
Ishan Goel <igoel.mail@gmail.com>
2025-09-04 19:28:43 -0400

    initial router, cstring, sdcard modules

---
+++ lib/data/SDCard.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SD.h>
+
+class SDCard {
+public:
+  static boolean begin();
+  static File open(const char *filename, char mode);
+  static String get_next_safe_name(const char *filename);
+
+private:
+  // used by comms
+  static void ls();
+  static void rm();
+  static void cat();
+  static void auto_cat();
+};
