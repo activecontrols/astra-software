208cb1a44f385fbba2d2b3cc5370e38f56629cf8
RobertJN64 <robertjnies@gmail.com>
2025-11-14 22:01:49 -0500

    vis done except for mag and internal state

--- UI/serial.cpp
+++ UI/serial.cpp
@@ -5,12 +5,17 @@
 
 HANDLE hSerial;
 state_packet_t state_packet;
-#define BUF_SIZE 1024
+#define BUF_SIZE 4096
+#define T_BUF_SIZE 1024
 
 // your message callback
 void handle_message(const char *msg) {
   if (msg[0] == '>') {
-    sscanf(msg, ">%f %f %f", &state_packet.acc_x, &state_packet.acc_y, &state_packet.acc_z);
+    sscanf(msg, ">%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",
+           &state_packet.accel_x, &state_packet.accel_y, &state_packet.accel_z, &state_packet.gyro_yaw, &state_packet.gyro_pitch, &state_packet.gyro_roll, &state_packet.mag_x, &state_packet.mag_y, &state_packet.mag_z, &state_packet.gps_pos_north, &state_packet.gps_pos_west, &state_packet.gps_pos_up, &state_packet.gps_vel_north, &state_packet.gps_vel_west, &state_packet.gps_vel_up,
+           &state_packet.state_q_vec_0, &state_packet.state_q_vec_1, &state_packet.state_q_vec_2, &state_packet.state_pos_north, &state_packet.state_pos_west, &state_packet.state_pos_up, &state_packet.state_vel_north, &state_packet.state_vel_west, &state_packet.state_vel_up, &state_packet.state_0, &state_packet.state_1, &state_packet.state_2, &state_packet.state_3, &state_packet.state_4, &state_packet.state_5,
+           &state_packet.gimbal_yaw_deg, &state_packet.gimbal_pitch_deg, &state_packet.thrust_N, &state_packet.roll_N,
+           &state_packet.target_pos_north, &state_packet.target_pos_west, &state_packet.target_pos_up);
   }
 }
 
@@ -19,7 +24,7 @@ void poll_serial() {
   static char buffer[BUF_SIZE];
   static int buf_len = 0;
 
-  char temp[256];
+  char temp[T_BUF_SIZE];
   DWORD bytes_read = 0;
 
   if (!ReadFile(hSerial, temp, sizeof(temp), &bytes_read, NULL)) {
