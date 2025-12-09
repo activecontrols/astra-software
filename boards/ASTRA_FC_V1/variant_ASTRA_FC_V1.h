d33450c9e46b774b9f3a224afc7407e9c61dbd87
RobertJN64 <robertjnies@gmail.com>
2025-12-08 22:51:54 -0500

    add board and variant files

---
+++ boards/ASTRA_FC_V1/variant_ASTRA_FC_V1.h
@@ -0,0 +1,5 @@
+#pragma once
+#define NUM_DIGITAL_PINS 10 // TODO - set this adjust to match your pins
+#define NUM_ANALOG_INPUTS 10
+#define Serial Serial1 // TODO - set this to the serial that connects out to USB
+#define HAVE_HWSERIAL1
