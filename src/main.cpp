2417e536eaeac6502de863cf9ad61e2d11ad806b
jacobh460 <jacobhillman016@gmail.com>
2025-12-10 17:44:35 -0500

    simple grid test (todo frequency sweep and circle test)

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,76 @@ void ping(const char *args) {
   Router::println(args == nullptr ? "null" : args);
 }
 
+
+void characterize_grid()
+{
+  float deg_min = -7.5;
+  float deg_max = 7.5;
+  const float interp_time = 2;
+
+  float a = -7.5;
+
+  IMU::Data last_packet;
+
+  Router::print("Angle A (deg), Angle B (deg), Accel X (m/s^2), Accel Y (m/s^2), Accel Z (m/s^2), Gyro X (rad/s), Gyro Y (deg/s), Gyro Z (deg/s)\n");
+
+  for (float b = -7.5; b <= 7.5; b += 0.2) {
+    unsigned long start_time = micros();
+
+    while (1)
+    {
+
+      IMU::IMUs[0].read_latest(&last_packet);
+
+      Router::printf("%.2f, %.2f, %.15lf, %.15lf, %.15lf, %.15lf, %.15lf, %.15lf\n", a, b, last_packet.acc[0], last_packet.acc[1], last_packet.acc[2], last_packet.gyro[0], last_packet.gyro[1], last_packet.gyro[2]);
+
+      unsigned long now_time = micros();
+      double t = (now_time - start_time) / 1000000.0;
+      if (t > interp_time) break;
+
+      a = (deg_max - deg_min) * (t / interp_time) + deg_min;
+
+      GimbalServos::setGimbalAngle(a, b);
+      delay(1);
+    }
+
+    float temp = deg_min;
+    deg_min = deg_max;
+    deg_max = temp;
+  }
+}
+
+
+void characterize_frequency(const char* param)
+{
+
+}
+
+void circle_test()
+{
+
+}
+
 void setup() {
   delay(3000);
   Router::begin();
   Router::println("Controller started.");
 
-  Prop::begin();
-  Mag::begin();
-  GPS::begin();
+  SPI.begin();
+
+  // Prop::begin();
+  // Mag::begin();
+  // GPS::begin();
   IMU::begin();
   GimbalServos::init();
-  TrajectoryLoader::begin();
-  TrajectoryFollower::begin();
+  // TrajectoryLoader::begin();
+  // TrajectoryFollower::begin();
 
   Router::add({ping, "ping"}); // example registration
   Router::add({Router::print_all_cmds, "help"});
+  Router::add({characterize_grid, "grid"});
+  Router::add({characterize_frequency, "frequency"});
+  Router::add({circle_test, "circle"});
 }
 
 void loop() {
