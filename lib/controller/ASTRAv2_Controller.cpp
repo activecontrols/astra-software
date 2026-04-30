da84240f3a6c041d5fa2ca0a89d4e8f6b8f6dfec
RobertJN64 <robertjnies@gmail.com>
2026-04-29 23:10:18 -0400

    preflight2

--- lib/controller/ASTRAv2_Controller.cpp
+++ lib/controller/ASTRAv2_Controller.cpp
@@ -41,7 +41,7 @@ Vector4 ASTRAv2_Controller(Vector3 PosTarget, Vector16 X, constantsASTRA_t const
   Vector3 PosError = PosTarget - X.segment<3>(4);
 
   // Velocity Command
-  Vector3 K_P = (Vector3() << 0.75, 0.75, 0.7).finished();
+  Vector3 K_P = (Vector3() << 0.75, 0.75, 0.65).finished();
   Vector3 VelTarget = K_P.cwiseProduct(PosError);
 
   // Velocity Saturation Step
@@ -53,13 +53,13 @@ Vector4 ASTRAv2_Controller(Vector3 PosTarget, Vector16 X, constantsASTRA_t const
   Vector3 VelError = VelTarget - X.segment<3>(7);
 
   // Integral Accumulator
-  Vector3 K_I = (Vector3() << 0.2, 0.2, 2).finished();
+  Vector3 K_I = (Vector3() << 0.2, 0.2, 1).finished();
   float Leak = 0.4;
   Vector3 Clamp = (Vector3() << 5, 5, 5).finished();
 
   // Normalize errors (0 to 1 scale)
   Vector3 MaxAttError = (Vector3() << 0.1, 0.1, 0.5).finished();
-  Vector3 MaxVelError = (Vector3() << 0.4, 0.4, 2).finished();
+  Vector3 MaxVelError = (Vector3() << 0.4, 0.4, 0.5).finished();
   Vector3 NormAttErr = lastAttError.cwiseAbs().cwiseQuotient(MaxAttError);
   Vector3 NormVelErr = VelError.cwiseAbs().cwiseQuotient(MaxVelError);
 
@@ -73,7 +73,7 @@ Vector4 ASTRAv2_Controller(Vector3 PosTarget, Vector16 X, constantsASTRA_t const
   K_I = K_I.cwiseProduct(Gate);
   VelErrorI = VelErrorI + K_I.cwiseProduct(VelError) * dT;
   VelErrorI = VelErrorI.cwiseMin(Clamp).cwiseMax(-Clamp);
-  K_P = (Vector3() << 2.6, 2.6, 5).finished();
+  K_P = (Vector3() << 2.4, 2.4, 2.5).finished();
 
   // Acceleration Target
   Vector3 AccelTarget = K_P.cwiseProduct(VelError) + VelErrorI + (Vector3() << 0, 0, constantsASTRA.g).finished();
